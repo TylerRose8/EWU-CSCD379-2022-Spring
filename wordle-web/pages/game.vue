58e69c43e71c361de30f2d1197e1f2bdc1e57edf
Tyler Rose <TylerRose25565@gmail.com>
2022-04-11 15:24:38 -0700

    Update game

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,10 +1,10 @@
 <template>
-    <v-row justify="center" align="center">
-        <v-card-title class="headline">
+    <h-row justify="center" align="center">
+        <h-card-title class="headline">
                 ǝlpɹoM
-        </v-card-title>
-        <v-spacer/>
-        <v-card>
+        </h-card-title>
+        <h-spacer/>
+        <h-card>
             <h-col cols="5" sm="8" md="6">
                 <h-card style="width:100px;height:100px">
                     X
@@ -22,20 +22,20 @@
                     X
                 </h-card>
             </h-col>
-        </v-card>
-        <v-card>
-        <v-card-text>
+        </h-card>
+        <h-card>
+        <h-card-text>
             Text goes here!?
-        </v-card-text>
-        </v-card>
-        <v-card>
+        </h-card-text>
+        </h-card>
+        <h-card>
             Keyboard goes here!!!
-        </v-card>
-        <v-card-actions>
-        <v-spacer/>
-        <v-btn color="primary" nuxt to="/"> Return Home </v-btn>
-        </v-card-actions>
-    </v-row>
+        </h-card>
+        <h-card-actions>
+        <h-spacer/>
+        <h-btn color="primary" nuxt to="/"> Return Home </h-btn>
+        </h-card-actions>
+    </h-row>
 </template>
 
 <script lang="ts">
