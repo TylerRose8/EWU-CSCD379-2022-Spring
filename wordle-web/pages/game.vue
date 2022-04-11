5a0b2b2d638d068b4dbb27bfb38ff6bebc6b4b76
Tyler Rose <TylerRose25565@gmail.com>
2022-04-11 15:00:29 -0700

    Update game page

---
+++ wordle-web/pages/game.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-row justify="center" align="center">
+        <v-col>
+            <h1>
+                ǝlpɹoM
+            </h1>
+        </v-col>
+        <v-col cols="5" sm="8" md="6">
+            <h-card style="width:100px;height:100px">
+                X
+            </h-card>
+            <h-card style="width:100px;height:100px">
+                X
+            </h-card>
+            <h-card style="width:100px;height:100px">
+                X
+            </h-card>
+            <h-card style="width:100px;height:100px">
+                X
+            </h-card>
+            <h-card style="width:100px;height:100px">
+                X
+            </h-card>
+        </v-col>
+        <v-col>
+            Text goes here!?
+        </v-col>
+        <v-col>
+            Keyboard goes here!!!
+        </v-col>
+    </v-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import Component from "vue-class-component"
+
+@Component
+export default class IndexPage extends Vue{
+    name: string = 'IndexPage';
+    buttonText: string = 'Inspire me!';
+    isLoading: boolean = false;
+
+    changeButtonText() {
+        this.buttonText = this.buttonText === 'Inspire me!' ? 'Inspire me again!' : 'Inspire me!';
+        this.isLoading = true;
+        setTimeout(() => {
+            this.isLoading = false;
+        }, 1000);
+    }
+}
+</script>
