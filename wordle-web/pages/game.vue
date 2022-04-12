690974c658d49937b15be9ccb14b0be88282168b
Tyler Rose <TylerRose25565@gmail.com>
2022-04-12 12:23:43 -0700

    Update game.vue

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,41 +1,29 @@
 <template>
-    <h-row justify="center" align="center">
-        <h-card-title class="headline">
+    <h1>
                 ǝlpɹoM 
-        </h-card-title>
-        <h-spacer/>
-        <h-card>
-            <h-col cols="5" sm="8" md="6">
-                <h-card style="width:100px;height:100px">
-                    X
-                </h-card>
-                <h-card style="width:100px;height:100px">
-                    X
-                </h-card>
-                <h-card style="width:100px;height:100px">
-                    X
-                </h-card>
-                <h-card style="width:100px;height:100px">
-                    X
-                </h-card>
-                <h-card style="width:100px;height:100px">
-                    X
-                </h-card>
-            </h-col>
-        </h-card>
-        <h-card>
-        <h-card-text>
-            Text goes here!?
-        </h-card-text>
-        </h-card>
-        <h-card>
-            Keyboard goes here!!!
-        </h-card>
-        <h-card-actions>
-        <h-spacer/>
-        <h-btn color="primary" nuxt to="/"> Return Home </h-btn>
-        </h-card-actions>
-    </h-row>
+    </h1>
+    <v-spacer/>
+    <div>
+        <div style="border:1px;width:100px;height:100px;">
+        <div style="border:1px;width:100px;height:100px;">
+        <div style="border:1px;width:100px;height:100px;">
+        <div style="border:1px;width:100px;height:100px;">
+        <div style="border:1px;width:100px;height:100px;">
+    </div>
+    <div>
+        Text goes here!?
+    </div>
+    </v-spacer/>
+    </v-spacer/>
+    <div>
+        Keyboard goes here!!!
+    </div>
+	<div>
+		<h-card-actions>
+			<h-spacer/>
+			<h-btn color="primary" nuxt to="/"> Return Home </h-btn>
+		</h-card-actions>
+	</div>
 </template>
 
 <script lang="ts">
