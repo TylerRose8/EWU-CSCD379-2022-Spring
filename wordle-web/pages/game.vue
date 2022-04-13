8ee115264d6d6aed825a797aac874ea20197d745
Tyler Rose <TylerRose25565@gmail.com>
2022-04-12 23:12:48 -0700

    Update Game

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,5 +1,26 @@
 <template>
 	<div>
+	<v-container class="grey lighten-5">
+    <v-row
+      v-for="n in 2"
+      :key="n"
+      :class="n === 1 ? 'mb-6' : ''"
+      no-gutters
+    >
+      <v-col
+        v-for="k in n + 1"
+        :key="k"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          {{ k }} of {{ n + 1 }}
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 		<h-row justify="center" align="center">
 			<h1>
 				ǝlpɹoM
