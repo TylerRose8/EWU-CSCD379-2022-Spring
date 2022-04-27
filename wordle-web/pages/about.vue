d7c9552519874d094880fb8538a7fda9735eae20
jphillips16 <jphillips16@eagles.ewu.edu>
2022-04-26 23:39:25 -0700

    Added in About Page with sample text and button

---
+++ wordle-web/pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-container fluid fill-height justify-center>
+        <v-card-text class="text-h4 font-weight-black text-center">
+            This is the simple About page text. Also Meg is awesome!!
+        </v-card-text>
+        <v-btn color="primary" nuxt to="/game"> Back To Game Page </v-btn>
+    </v-container>
+</template>
