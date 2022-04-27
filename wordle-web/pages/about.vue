9a7090e2e20ccdbc8d9878f2ab2bbbed4a230fd7
jphillips16 <jphillips16@eagles.ewu.edu>
2022-04-27 00:09:02 -0700

    Format Updates

--- wordle-web/pages/about.vue
+++ wordle-web/pages/about.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container fluid fill-height justify-center>
-        <v-card-text class="text-h4 font-weight-black text-center">
+        <v-card-text class="text-h5 font-weight-black text-center">
             This is the simple About page text.
             With this project, we are creating Wordle as a class.
             You can play Wordle as much as you want on this amazing site!
