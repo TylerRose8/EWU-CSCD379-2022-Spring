363a63b316d137c1fc304296da788f971794c977
jphillips16 <jphillips16@eagles.ewu.edu>
2022-04-27 00:01:18 -0700

    Modified About Page text

--- wordle-web/pages/about.vue
+++ wordle-web/pages/about.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container fluid fill-height justify-center>
-        <v-card-text class="text-h4 font-weight-black text-center">
+        <v-card-text class="text-h5 font-weight-light text-center">
             This is the simple About page text. Also Meg is awesome!!
         </v-card-text>
         <v-btn color="primary" nuxt to="/game"> Back To Game Page </v-btn>
