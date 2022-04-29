fd2fbb775967504cd5e658126041132051fb2b23
Tyler Rose <trose8@ewu.edu>
2022-04-29 00:00:00 -0700

    Updates

--- wordle-web/components/available-wordlist.vue
+++ wordle-web/components/available-wordlist.vue
@@ -1,16 +1,18 @@
 <template>
   <v-card class="my-5 pa-5">
-    <v-list style="max-height:200px" class="overflow-y-auto overflow-x-hidden">
-    <v-row>
-      <v-container class="text-left" style="position:absolute">
-        Possible: {{wordleGame.viewHints.length === undefined? 0 : wordleGame.viewHints.length}}
-      </v-container>
-    </v-row>
-    <v-row v-for="(word, i) in wordleGame.viewHints" :key="i" no-gutters justify="center">
-      <v-container class="text-center">
-        <v-btn :disabled="wordleGame.gameOver" @click="setWord(word)"> {{ word }}</v-btn>
-      </v-container>
-    </v-row>
+    <v-list style="max-height:200px;padding-top:20px" class="overflow-y-auto overflow-x-hidden">
+      <v-row>
+        <v-container class="text-left" style="position:absolute">
+          Possible: {{ wordleGame.viewHints.length === undefined ? 0 : wordleGame.viewHints.length }}
+        </v-container>
+      </v-row>
+      <v-row v-for="(word, i) in wordleGame.viewHints" :key="i" no-gutters justify="center">
+        <v-col cols="4">
+          <v-container class="text-center">
+            <v-btn :disabled="wordleGame.gameOver" @click="setWord(word)"> {{ word }}</v-btn>
+          </v-container>
+        </v-col>
+      </v-row>
     </v-list>
   </v-card>
 </template>
@@ -21,7 +23,7 @@ import {Component, Vue, Prop} from 'vue-property-decorator'
 import {WordleGame} from '~/scripts/wordleGame'
 
 
-@Component({components:{}})
+@Component({components: {}})
 export default class AvailableWordlist extends Vue {
   @Prop({required: true})
   wordleGame!: WordleGame
