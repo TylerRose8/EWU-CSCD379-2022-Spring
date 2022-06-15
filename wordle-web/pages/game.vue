cc8ad69ddb08f5f457f1de2f8fad3745f387d1e9
Tyler Rose <trose8@ewu.edu>
2022-06-14 22:25:17 -0700

    Remove play again button

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -83,13 +83,6 @@
           </v-card-text>
         </v-col>
       </v-row>
-      <v-row justify="center" class="mt-10">
-        <v-alert v-if="wordleGame.gameOver" width="80%" :type="gameResult.type">
-          {{ gameResult.text }}
-          <v-btn class="ml-2" @click="resetGame"> Play Again? </v-btn>
-        </v-alert>
-      </v-row>
-
       <v-row justify="center">
         <game-board :wordleGame="wordleGame" />
       </v-row>
